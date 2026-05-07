4a73ba071e983869e75ccc0c43dc2498104557e1
sadiq t <staslimrt@gmail.com>
2026-05-06 22:42:08 -0400

    wired CreateCaseModal to the dashboard, now cases can be added with the click of a button

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect } from "react"
+import CreateCaseModal from '../components/CreateCaseModal';
 
 interface Case {
   id: number;
@@ -18,33 +19,31 @@ function DashboardPage() {
   const [cases, setCases] = useState<Case[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const {token} = useAuth();
   const fetchCases = async () => {
-    
+    try {
+      const response = await axios.get('http://localhost:8000/api/cases', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCases(response.data);
+
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        setError(err.response.data.error || 'Failed to fetch cases');
+      } else {
+        setError('Network error');
+      }
+      
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
-    const fetchCases = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/api/cases', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        setCases(response.data);
-
-      } catch (err) {
-        if (axios.isAxiosError(err) && err.response) {
-          setError(err.response.data.error || 'Failed to fetch cases');
-        } else {
-          setError('Network error');
-        }
-        
-      } finally {
-        setIsLoading(false);
-      }
-    };
     fetchCases();
   }, [token]);
 
@@ -56,12 +55,21 @@ function DashboardPage() {
   }
   return (
     <div>
-    {cases?.map(c => ( 
-      <div key={c.id}>
-        <strong>{c.name}</strong> - {c.severity} - {c.status}
+      <button onClick={() => setIsModalOpen(true)}>New Case</button>
+
+      {cases?.map(c => ( 
+        <div key={c.id}>
+          <strong>{c.name}</strong> - {c.severity} - {c.status}
+        </div>
+      ))}
+
+      {isModalOpen && (
+        <CreateCaseModal
+          onClose={() => setIsModalOpen(false)}
+          onSuccess={fetchCases}
+          />
+        )}
       </div>
-    ))}
-    </div>
-  );
+    );
 }
   export default DashboardPage;
